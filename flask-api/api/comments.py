74a2fb605608054aa30ffb4aeb5bee4bfb4a93e4
adrian <adrianfj@gmail.com>
2019-08-02 15:33:23 -0400

    Resructure project

--- flask-api/api/comments.py
+++ flask-api/api/comments.py
@@ -1,9 +1,14 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, jsonify
 from flask_restful import reqparse, abort, Api, Resource
 
+# Initiate app
 app = Flask(__name__)
 api = Api(app)
 
+# set config
+app.config.from_object('project.config.DevelopmentConfig')
+
+# Temporary data
 COMMENTS = {
     'cm1': {'title': 'First comment', 'text' : 'first text'},
     'cm2': {'title': '2nd comment', 'text' : 'not much text'},
@@ -15,10 +20,18 @@ parser.add_argument('title', type=str)
 parser.add_argument('text', type=str)
 
 class CommentsList(Resource):
+
     def get(self):
-        return COMMENTS
+        """ Get a list of all existing comments"""
+        response_object = {
+            'status': 'success',
+            'message': f'List of comments',
+            'data': COMMENTS
+        }
+        return response_object, 200
 
     def post(self):
+        """ Add a new comment"""
         args = parser.parse_args(strict=True)
         # post_data = request.get_json()
         # title = post_data.get('title')
@@ -36,6 +49,7 @@ class CommentsList(Resource):
         }
         return response_object, 201
 
+# Add routing
 api.add_resource(CommentsList, '/')
 
 
