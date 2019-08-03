df36338a5e66bbd57add90141ff3ef39a4d2706d
adrian <adrianfj@gmail.com>
2019-08-03 14:10:31 -0400

    Integrate socketio with Flask

--- flask-api/project/app.py
+++ flask-api/project/app.py
@@ -1,7 +1,6 @@
-from flask import Flask, render_template, session, request, \
-    copy_current_request_context
-from flask_socketio import SocketIO, emit, join_room, leave_room, \
-    close_room, rooms, disconnect
+from threading import Lock
+from flask import Flask, render_template, session 
+from flask_socketio import SocketIO, emit
 
 # Enable async_mode in convination with "eventlet" or "gevent"
 # Set this variable to "threading"
@@ -15,7 +14,18 @@ thread_lock = Lock()
 
 @app.route('/')
 def index():
+    """ Endpoint to serve index.html """
     return render_template('index.html', async_mode=socketio.async_mode)
+
+
+@socketio.on('my_comments_event', namespace='/test')
+def broadcast_comments(message):
+    """ Namespace channel to recieve and emit comments from and to client"""
+    session['receive_count'] = session.get('receive_count', 0) + 1
+    emit('my_response',
+         {'data': message['data'], 'count': session['receive_count']},
+         broadcast=True)
+
 
 if __name__ == '__main__':
     socketio.run(app, debug=True)
