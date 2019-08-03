7fc4e9c9d2fd4c43277fa7984aca43bc211f64ff
adrian <adrianfj@gmail.com>
2019-08-03 15:42:59 -0400

    Update models

--- flask-api/project/models.py
+++ flask-api/project/models.py
@@ -9,12 +9,12 @@ class Comments(db.Model):
     title = db.Column(db.String, nullable=False)
     text = db.Column(db.String, nullable=False)
     # pub_date = 
-    # username
-    # email
+    # username = db.Column(db.String(80), unique=True, nullable=False)
+    # email = db.Column(db.String(120), unique=True, nullable=False)
 
     def __init__(self, title, text):
         self.title = title
         self.text = text
 
     def __repr__(self):
-        return '<title {}>'.format(self.body)
+        return '<title {}>'.format(self.title)
