13289ec9b9e2ee67fc89cb55d4a917e9c1ca63c8
adrian <adrianfj@gmail.com>
2019-08-04 11:52:56 -0400

    Update Comments model

--- flask-com/models.py
+++ flask-com/models.py
@@ -1,20 +1,21 @@
 from app import db
+from datetime import datetime
 
 
 class Comments(db.Model):
 
     __tablename__ = "comments"
 
-    uid = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String, nullable=False)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     text = db.Column(db.String, nullable=False)
-    # pub_date = 
-    # username = db.Column(db.String(80), unique=True, nullable=False)
-    # email = db.Column(db.String(120), unique=True, nullable=False)
+    username = db.Column(db.String(128), nullable=False)
+    email = db.Column(db.String(128), nullable=False)
+    pub_date = db.Column(db.DateTime, default=datetime.utcnow())
 
-    def __init__(self, title, text):
-        self.title = title
+    def __init__(self, username, text, email):
+        self.username = username
         self.text = text
+        self.email = email
 
     def __repr__(self):
-        return '<title {}>'.format(self.title)
+        return f'<comment {self.id} by {self.username}>'
